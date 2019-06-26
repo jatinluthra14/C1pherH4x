e3a7bd4a0f194580712e9ea75d7d1ddb2b570476
Jatin Luthra <jatinluthra14@gmail.com>
2019-06-27 01:12:20 +0530

    Finish bacon cipher and complete restructure

--- C1pherH4x.py
+++ C1pherH4x.py
@@ -1,26 +1,87 @@
 import base64
 import argparse
+import re
+from ciphers import bacon
+
+ciphers_list = {'bacon': bacon}
 
 class C1pherH4x:
-    def __init__(self, ciphertext, flag_format):
+    def __init__(self, plaintext=None, ciphertext=None, flag_format=None, cipher=None):
+        self.plaintext = plaintext
         self.ciphertext = ciphertext
         self.flag_format = flag_format
+        self.cipher = cipher
+
+    def encode(self):
+        if self.cipher:
+            if self.cipher in ciphers_list:
+                self.ciphertext = ciphers_list[self.cipher].encode(self.plaintext)
+                if self.ciphertext:
+                    print("Ciphertext:")
+                    print(self.ciphertext)
+            else:
+                print(f"Cipher {self.cipher} does not exist!")
+                exit(0)
+        else:
+            print("No Cipher provided to encode")
+            exit(0)
+            
+    def decode(self):
+        if self.cipher:
+            if self.cipher in ciphers_list:
+                self.plaintext = ciphers_list[self.cipher].decode(self.ciphertext)
+                if self.plaintext:
+                    print("Found Plaintext: ")
+                    print(self.plaintext)
+            else:
+                print(f"Cipher {self.cipher} does not exist!")
+                exit(0)
+        else:
+            print("Detecting Cipher")
+            self.detect_ciphers()
+
+    def detect_ciphers(self):
+        if re.match('[A|a]*[B|b]*' , self.ciphertext):
+            print("Seems like Bacon Cipher")
+            self.plaintext = bacon.decode(self.ciphertext)
+        if self.plaintext:
+            print("Found Plaintext: ")
+            print(self.plaintext)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ct', '--ciphertext', help='The ciphertext to use against')
-    parser.add_argument('-cf', '--cipherfile', help='The file which contains the ciphertext to use against')
+    parser.add_argument('-ct', '--ciphertext', help='The ciphertext to decode')
+    parser.add_argument('-cf', '--cipherfile', help='The file which contains the ciphertext to decode')
+    parser.add_argument('-pt', '--plaintext', help='The plaintext to encode')
+    parser.add_argument('-pf', '--plainfile', help='The file which contains the plaintext to encode')
     parser.add_argument('-ff', '--flag-format', help='The known flag format(regex) to search for, otherwise displays everything')
-    parser.add_argument('--attacks', help='Attacks to implement, Available: base64, brutexor, morse')
+    parser.add_argument('-c', '--cipher', help='Ciphers to implement, Available: base64, brutexor, morse')
+    parser.add_argument('-e', '--encode', help='Use to encode plaintext with given cipher', action='store_true')
+    parser.add_argument('-d', '--decode', help='Use to decode ciphertext with given cipher or detect cipher', action='store_true')
     args = parser.parse_args()
 
-    if args.cipherfile:
-        ciphertext = open(args.cipherfile, 'r').read()
+    if args.encode:
+        if args.plainfile:
+            plaintext = open(args.plainfile, 'r').read()
+        elif args.plaintext:
+            plaintext = args.plaintext
+        else:
+            print('Please mention plaintext/plainfile')
+            exit(0)
+        plain = C1pherH4x(plaintext=plaintext, cipher=args.cipher)
+        plain.encode()
+    elif args.decode:
+        if args.cipherfile:
+            ciphertext = open(args.cipherfile, 'r').read()
+        elif args.ciphertext:
+            ciphertext = args.ciphertext
+        else:
+            print('Please mention ciphertext/cipherfile')
+            exit(0)
+        cipher = C1pherH4x(ciphertext=ciphertext, flag_format=args.flag_format, cipher=args.cipher)
+        cipher.decode()
     else:
-        ciphertext = args.ciphertext
-
-    if not ciphertext:
-        print("Please input a ciphertext")
+        print('Encode/Decode not mentioned')
         exit(0)
     
-    cipher = C1pherH4x(ciphertext, args.flag_format)
---
+++ ciphers/bacon.py
@@ -0,0 +1,40 @@
+table = {'A':'AAAAA', 'B':'AAAAB', 'C':'AAABA', 'D':'AAABB', 'E':'AABAA', 'F':'AABAB',
+            'G':'AABBA', 'H':'AABBB', 'I':'ABAAA', 'J':'ABAAB', 'K':'ABABA', 'L':'ABABB', 
+            'M':'ABBAA', 'N':'ABBAB', 'O':'ABBBA', 'P':'ABBBB', 'Q':'BAAAA', 'R':'BAAAB', 
+            'S':'BAABA', 'T':'BAABB', 'U':'BABAA', 'V':'BABAB', 'W':'BABBA', 'X':'BABBB', 
+            'Y':'BBAAA', 'Z':'BBAAB'}
+table2 = {'A':'AAAAA', 'B':'AAAAB', 'C':'AAABA', 'D':'AAABB', 'E':'AABAA', 'F':'AABAB',
+            'G':'AABBA', 'H':'AABBB', 'J':'ABAAA', 'I':'ABAAA', 'K':'ABAAB', 'L':'ABABA', 'M':'ABABB', 
+            'N':'ABBAA', 'O':'ABBAB', 'P':'ABBBA', 'Q':'ABBBB', 'R':'BAAAA', 'S':'BAAAB', 
+            'T':'BAABA', 'V':'BAABB', 'U':'BAABB', 'W':'BABAA', 'X':'BABAB', 'Y':'BABBA', 'Z':'BABBB', }
+dec_table = {v: k for k, v in table.items()} 
+dec_table2 = {v: k for k, v in table2.items()} 
+
+def decode(ciphertext):
+    ciphertext = ciphertext.upper()
+    length = len(ciphertext)
+    curr = 0
+    plaintext = ['By 24 letter Bacon: ']
+    plaintext2 = ['By 26 letter Bacon: ']
+    while curr in range(length):
+        if ciphertext[curr] == " ":
+            plaintext.append(" ")
+            plaintext2.append(" ")
+            curr += 1
+        else:
+            char = ciphertext[curr:curr+5]
+            plaintext.append(dec_table[char])
+            plaintext2.append(dec_table2[char])
+            curr += 5
+    plaintexts = ["".join(plaintext), "".join(plaintext2)]
+    return "\n".join(plaintexts)
+
+def encode(plaintext):
+    plaintext = plaintext.upper()
+    ciphertext = ['By 24 letter Bacon: ']
+    ciphertext2 = ['By 26 letter Bacon: ']
+    for char in plaintext:
+        ciphertext.append(table[char])
+        ciphertext2.append(table2[char])
+    ciphertexts = ["".join(ciphertext), "".join(ciphertext2)]
+    return "\n".join(ciphertexts)
